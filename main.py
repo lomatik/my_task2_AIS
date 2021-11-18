82fa26690e0bb9413c5444f443845ae0b4af80c5
lomatik <ralenko2012@gmail.com>
2021-11-18 14:14:47 +0200

    First part DONE

--- main.py
+++ main.py
@@ -2,25 +2,43 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 
 testcsv = pd.read_csv('house-prices-advanced-regression-techniques/test.csv');
-sample_submissioncsv = pd.read_csv('house-prices-advanced-regression-techniques/sample_submission.csv');
 traincsv = pd.read_csv('house-prices-advanced-regression-techniques/train.csv');
 
-#print(sample_submissioncsv)
 print(traincsv)
 
-z = traincsv.loc[:, traincsv.columns != 'SalePrice']
+print ("Train data shape:", traincsv.shape) # sizes of data
+print ("Test data shape:", testcsv.shape)
 
-X = np.array(z['MSSubClass'])#['LotArea']
-X = X.reshape(1, -1)
-y = np.dot(X, traincsv['SalePrice'])
+data = traincsv.select_dtypes(include=[np.number]).interpolate().dropna() #interpolating data from train
 
-reg = linear_model.LinearRegression().fit(X, y)
+y = np.log(traincsv.SalePrice)
+X = data.drop(['SalePrice', 'Id'], axis=1)
 
-print('REG_PRINT')
+X_train, X_test, y_train, y_test = train_test_split(
+                          X, y, random_state=42, test_size=.33)
+lr = linear_model.LinearRegression()
+model = lr.fit(X_train, y_train)
+print ("TRAIN_SCORE_R^2 is: \n", model.score(X_train, y_train))
+print ("TEST_SCORE_R^2 is: \n", model.score(X_test, y_test))
+predictions = model.predict(X_test)
 
-print(reg.coef_)
-print(reg.intercept_)
+print ('RMSE is: \n', mean_squared_error(y_test, predictions))
+
+actual_values = y_test
+plt.scatter(predictions, actual_values, alpha=.7,
+            color='b') #alpha helps to show overlapping data
+plt.xlabel('Predicted Price')
+plt.ylabel('Actual Price')
+plt.title('Linear Regression Model')
+plt.show()
+
+#Second part
+
+
 
