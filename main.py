7b27cce681c4c958fd27951be0d95e6beb003b1c
lomatik <ralenko2012@gmail.com>
2021-11-19 10:58:35 +0200

    third part done

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import graphviz
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,7 +9,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn import tree
 from sklearn import preprocessing
-from sklearn.datasets import load_iris
+from sklearn.cluster import KMeans
 
 testcsv = pd.read_csv('house-prices-advanced-regression-techniques/test.csv');
 traincsv = pd.read_csv('house-prices-advanced-regression-techniques/train.csv');
@@ -35,12 +35,12 @@ predictions = model.predict(X_test)
 print ('RMSE is: \n', mean_squared_error(y_test, predictions))
 
 actual_values = y_test
-plt.scatter(predictions, actual_values, alpha=.7,
-            color='b') #alpha helps to show overlapping data
-plt.xlabel('Predicted Price')
-plt.ylabel('Actual Price')
-plt.title('Linear Regression Model')
-plt.show()
+#plt.scatter(predictions, actual_values, alpha=.7,
+ #           color='b') #alpha helps to show overlapping data
+#plt.xlabel('Predicted Price')
+#plt.ylabel('Actual Price')
+#plt.title('Linear Regression Model')
+#plt.show()
 
 #Second part
 
@@ -56,9 +56,31 @@ clf = clf.fit(x2, y2e)
 clf.predict(X_test)
 
 clf.predict_proba(X_test)
-tree.plot_tree(clf)
+#tree.plot_tree(clf)
 r = tree.export_text(clf)
 print(r)
 
 
 #third part
+
+kmeans = KMeans(n_clusters=5, random_state=0).fit(X_train)
+
+print(kmeans.labels_)
+print(kmeans.predict(X_test))
+print(kmeans.cluster_centers_.shape)
+
+y_kmeans = kmeans.predict(X_test)
+
+print(X_test.shape)
+print(y_kmeans.shape)
+
+#plt.scatter(X_test[:, 0], X_test[:, 1], c = y_kmeans, s = 20)
+
+X_output = np.array(X_test)
+
+print(X_output.shape)
+
+plt.scatter(X_output[:, 0], X_output[:, 1], c = y_kmeans, s = 20, cmap = 'summer')
+centers = kmeans.cluster_centers_
+plt.scatter(centers[:, 0], centers[:, 1], c = 'blue', s = 100, alpha = 0.9);
+plt.show()
